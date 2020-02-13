c81aa6ee9a4345288e2e8d8c550f6da34327f18e
iwanbazz <iwan.bazz@gmail.com>
2020-02-13 19:13:47 +0700

    heroku deploy

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,4 @@
 <?php
-$connecting = mysqli_connect('localhost','root','');
-$database = mysqli_select_db($connecting,'db_kpinventory');
 
-?>
+$connecting = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'ba4cb6cb3b4202', '745cb2f6');
+$database = mysqli_select_db($connecting, 'heroku_192737218e1a705');
